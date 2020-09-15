c41bd9bd8aed816bb13fa79703590bb813c1fe5b
Hoang <lequanghoang99@outlook.com>
2020-09-15 10:00:41 +0700

    Update project v1.0.3

--- generate.sh
+++ generate.sh
@@ -5,13 +5,16 @@ create /database/name/user "CashBagUser"
 create /database/name/transaction "CashBagTransaction"
 create /database/name/company "CashBagCompany"
 create /database/test
-create /database/test/name "CashBag-test"
+create /database/test/user "CashBagUserTest"
+create /database/test/transaction "CashBagTransactionTest"
+create /database/test/company "CashBagCompanyTest"
 create /redis
 create /redis/uri "redis:6379"
 create /redis/pass ""
 create /app/port/user ":8082"
 create /app/port/company ":8081"
 create /app/port/transaction ":8080"
+create /grpc
 create /grpc/address
 create /grpc/address/user "localhost"
 create /grpc/address/company "localhost"
